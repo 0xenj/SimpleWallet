eebbb3c977c62ef530b90503b2dfeae7512d2e3b
enzo jakobasch <enzo.jakobasch@gmail.com>
2023-04-09 15:20:06 +0200

    improve staking

--- src/SimpleWallet.sol
+++ src/SimpleWallet.sol
@@ -4,19 +4,30 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract SimpleWallet {
-    address payable public owner;
+    address public owner;
     IERC20 public immutable token;
     IERC20 public immutable tokenRewards;
     uint256 public totalWalletsFunds;
+    uint256 public totalStaking;
+    // time funds are lock to stake
+    uint32 public duration;
 
     mapping(address => uint256) public balances;
+    mapping(address => uint256) public stake;
+    mapping(address => uint256) public stakeLock;
 
     /* Token will be Sepolia ETH
        TokenReward will be a custom token */
-    constructor(address _token, address _tokenRewards) {
-        owner = payable(msg.sender);
+    constructor(address _token, address _tokenRewards, uint32 _duration) {
+        owner = msg.sender;
         token = IERC20(_token);
         tokenRewards = IERC20(_tokenRewards);
+        duration = _duration;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "not authorized");
+        _;
     }
 
     function deposit(uint256 _amount) external {
@@ -44,12 +55,37 @@ contract SimpleWallet {
         token.transferFrom(address(this), msg.sender, _amount);
     }
 
-    function stacking() external {
+    function stacking(uint256 _amount) external {
+        require(_amount > 0, 'amount = 0');
+        require(balances[msg.sender] >= _amount, 'Not enough funds on wallet');
 
+        balances[msg.sender] -= _amount;
+        stake[msg.sender] += _amount;
+        stakeLock[msg.sender] += block.timestamp + duration;
+        totalStaking += _amount;
+
+    }
+
+    function unStaking() external {}
+
+    function setDuration(uint32 _duration) external onlyOwner {
+        duration = _duration;
+    }
+
+    function getDuration() public view returns (uint32) {
+        return duration;
     }
 
     function getBalance() public view returns (uint256) {
         return balances[msg.sender];
+    }
+
+    function getStaking() public view returns (uint256) {
+        return stake[msg.sender];
+    }
+
+    function getTotalStaking() public view returns (uint256) {
+        return totalStaking;
     }
 
     function getTotalWalletsFunds() public view returns (uint256) {
